ae43c4e5ef927420fb8c7cf6111bd7c8fbc5d4de
D-vid-push <davidfriday176@gmail.com>
2024-07-18 19:16:44 +0100

    feat: add changes on the pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import Navbar from './NavbarPage/NavbarPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <App />
+ <div className="flex bg-[#000000] text-[#FFFFFF]">
+ <Navbar />
+ <App />
+ </div>
 </BrowserRouter>,
+
+{/* <React.StrictMode>
+     <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  </React.StrictMode> */}
 );
 
 // If you want to start measuring performance in your app, pass a function
